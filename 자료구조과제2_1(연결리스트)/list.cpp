e541adb2ef5428c778c622e14dc360d40e0d843c
glitterer <50728605+glitterer@users.noreply.github.com>
2021-05-01 02:49:56 +0900

    Create list.cpp

---
+++ 자료구조과제2_1(연결리스트)/list.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "list.h"
+using namespace std;
+
+void LinkedList::Append(int data) // List의 마지막에 새로운 data에 대한 Node 추가
+{
+	//노드생성
+	Node* newNode = new Node();
+	newNode->data = data;
+	newNode->next = NULL;
+
+	//노드 추가
+	if (head == NULL) { 
+		head = newNode; 
+	}
+	else { 
+		newNode->next = head;
+		head = newNode;
+	}
+	
+}
+
+int LinkedList::Length() // List에 있는 Node의 수를 return
+{
+	int i = 0;
+	while (head != NULL) {
+		i++;
+		head = head->next;
+	}
+	return i;
+}
+
+//거꾸로 출력되는 오류가 발생... 1 2 3->3 2 1로 출력됨
+void LinkedList::Print() // List에 있는 모든 Node를 head부터 순서대로 출력되어야 함
+{
+	while (head != NULL)
+	{
+		cout << head->data << " "; //디버깅 할 때, 계속 여기서 문제가 발생(length()도 마찬가지...0xccccc오류)
+		head = head->next;
+	}
+}
+
